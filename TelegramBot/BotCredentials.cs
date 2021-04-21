f1da9ed11da8bd4231e0173e7f5f29234401b31e
Alexey Sorokin <asorokin@ukaachen.de>
2021-04-21 11:41:02 +0200

    Add project files.

---
+++ TelegramBot/BotCredentials.cs
@@ -0,0 +1,7 @@
+﻿namespace TelegramBot
+{
+    public static class BotCredentials
+    {
+        public static readonly string BotToken = "1700123782:AAGTzfO5AboalMH_Txdan5VYaa2s5pPRibU";
+    }
+}
---
+++ TelegramBot/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TelegramBot
+{
+    using System;
+    using Telegram.Bot;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var botClient = new TelegramBotClient(BotCredentials.BotToken);
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine("Hello my name is {0}.", me.FirstName);
+        }
+    }
+}
